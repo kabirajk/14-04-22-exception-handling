14bfb7e945adce2b2e2a6b60c8c971e899abe2f2
kabirajk <kabiraj.mc19@bitsathy.ac.in>
2022-04-15 11:15:01 +0530

    ile handiling exception

---
+++ Fstream.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+//check a file is present or not
+int main () 
+{
+  fstream myfile;//input file stream
+  try
+  {
+      myfile.open("mytext.txt");
+      if (!myfile.is_open())
+        throw "!File not found";//throwing string
+  }
+  catch(const char *c)
+  {//
+      cerr<<c;//"File not found";
+
+      //output file stream
+      ofstream myfile("mytext.txt");//ostream create afile named mytext if not found
+  }
+  
+  myfile<<"//this text is insterted by using fstream";
+  myfile<<"\n//this text is insterted by using fstream";
+  myfile.close();
+
+  ifstream input;
+  try
+  {
+    input.open("mytext.txt");
+    if(!input.is_open())
+        throw "File not found istream";
+
+
+    //this statement read the file char by char until the eof//end of file comes
+  cout<<"\n#==============textfile================#"<<endl;
+    while (!input.eof())
+        {
+          cout<<(char)input.get();
+        }
+  cout<<"\n#=======================================#"<<endl;
+    
+    //this satement read line by line so it neads a string to fold the line
+
+    //!!!use get or getline at a time dont use both //!!clarify on this 
+  cout<<"\n#==============textfile================#"<<endl;
+
+    string line;//line holds the whole everyline
+    while (getline(input,line))
+        cout<<line<<endl;//a single is at a ones a time soline holds the last line after iterations
+    //this reads until the last 
+    //getline act as cin for reading the lines in text(file) so a string is needed to pass
+    //now line has the whole files string in it
+  cout<<"\n#=======================================#"<<endl;
+  
+    input.close();
+  }
+  catch(const char * c)
+  {
+    cerr<<c;
+  }
+  cout<<"\nend file closed"<<endl;
+return 0;
+}
